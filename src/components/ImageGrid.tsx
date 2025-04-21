2a8454bbebfe0236392a599e3d200eb79e0fd2dd
Tanveer Shahriar Arnob <tanveershahriara@gmail.com>
2025-04-21 21:57:07 +0600

    image grid bg

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -16,7 +16,20 @@ const ImageGrid = () => {
   ];
 
   return (
-    <section className="relative py-16 px-16 bg-[url('/images/BG_Blur.png')] bg-cover bg-center">
+    <section className="relative px-16">
+      <div className="absolute inset-0 flex justify-center items-center z-0">
+        <div
+          className="bg-[url('/images/BG_Blur.png')] bg-cover bg-center rounded-full"
+          style={{
+            width: '1600px',  
+            height: '1600px', 
+            position: 'absolute',  
+            top: '50%', 
+            left: '50%', 
+            transform: 'translate(-50%, -50%)',
+          }}
+        ></div>
+      </div>
       <div className="grid grid-cols-6 gap-4 relative z-10">
         {images.map((src, index) => (
           <div
