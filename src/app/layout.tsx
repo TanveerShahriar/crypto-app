7f0da07164325336816caaa8fa7563555d88ce21
Tanveer Shahriar Arnob <tanveershahriara@gmail.com>
2025-04-21 06:28:16 +0600

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Image from 'next/image';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,10 +25,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header className="fixed top-10 left-1/2 transform -translate-x-1/2 z-50 p-5 bg-[rgba(255,255,255,0.06)] backdrop-blur-[20px] flex justify-between items-center text-white shadow-lg w-[80%] rounded-full">
+          <div className="ml-8">
+            <Image 
+                aria-hidden
+                src="/images/Logo.png"
+                alt="Logo icon"
+                width={36}
+                height={50}
+              />
+          </div>
+          <nav className="flex space-x-20 flex-grow justify-center">
+            <a href="#" className="hover:text-gray-300 font-inter font-normal text-2xl leading-none tracking-normal text-center">Home</a>
+            <a href="#" className="hover:text-gray-300 font-inter font-normal text-2xl leading-none tracking-normal text-center">Features</a>
+            <a href="#" className="hover:text-gray-300 font-inter font-normal text-2xl leading-none tracking-normal text-center">Career</a>
+          </nav>
+          <div>
+            <button className="px-12 py-4 text-black text-xl font-bold bg-[#aeaeae] rounded-[30px] hover:bg-[#8c8c8c]">
+              Register
+            </button>
+          </div>
+        </header>
+
+        <main className="px-45">
+          {children}
+        </main>
       </body>
     </html>
   );
