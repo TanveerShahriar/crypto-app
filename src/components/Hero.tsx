f3efe7f7be670e79685a9a6aa78edc6b5d91216d
Tanveer Shahriar Arnob <tanveershahriara@gmail.com>
2025-04-22 22:53:55 +0600

    register link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 const Hero = () => {
     return (
       <section className="text-white py-20 px-6 text-center">
@@ -8,13 +10,16 @@ const Hero = () => {
           Ready to energize your life and turn it into earnings? With Move to Earn, your steps hold real value. Simply walk, run, or exercise to earn. Each step matters.
         </p>
         <div className="flex justify-center space-x-4 mt-16">
+          <Link href="/register"> 
             <button className="w-[200px] h-[66px] text-2xl bg-[rgba(126,126,126,0.29)] border-[1px] border-[rgba(255,255,255,0.2)] rounded-[12px] backdrop-blur-[20px] hover:bg-[rgba(126,126,126,0.4)] focus:outline-none focus:ring-2 focus:ring-gray-400">
                 Sign Up
             </button>
-
+          </Link>
+          <Link href="/register">
             <button className="w-[200px] h-[66px] text-2xl rounded-[12px] px-8 py-3 bg-white text-gray-600 rounded-lg border border-gray-300 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-400">
                 Login
             </button>
+          </Link>
         </div>
       </section>
     );
