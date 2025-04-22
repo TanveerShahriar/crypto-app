c47864af94cd1f2d6a0b3b571f1dfaa95a8ac14e
Tanveer Shahriar Arnob <tanveershahriara@gmail.com>
2025-04-22 23:06:17 +0600

    done hopefully again

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -27,6 +27,7 @@ const ImageGrid = () => {
             top: '50%', 
             left: '50%', 
             transform: 'translate(-50%, -50%)',
+            pointerEvents: 'none',
           }}
         ></div>
       </div>
